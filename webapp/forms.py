d3b127906ee4c0cbc6ba4f14e4181ed4fb157a55
Mateus Pereira <mdevino.coding@gmail.com>
2019-06-02 12:42:20 +0000

    Blue styling.

--- webapp/forms.py
+++ webapp/forms.py
@@ -1,7 +1,12 @@
-from django.forms import ModelForm
+from django import forms
 from .models import Document
 
-class DocumentForm(ModelForm):
+class DocumentForm(forms.ModelForm):
 	class Meta:
 		model = Document
 		fields = ['content']
+	
+	def __init__(self, *args, **kwargs):
+		super(DocumentForm, self).__init__(*args, **kwargs)
+		self.fields['content'].widget = forms.Textarea(attrs={'id': 'my_input'})
+	
